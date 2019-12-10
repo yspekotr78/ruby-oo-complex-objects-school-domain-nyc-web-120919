aea0376902099183b5fa69d3bf655fbee31a4bc8
Yury Spektor <yspektor78@gmail.com>
2019-12-10 17:24:51 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,43 @@
 # code here!
+require 'pry'
+class School
+  def initialize(school)
+    @school_name = school
+    @roster={}
+  end
+  def roster
+    @roster
+  end
+  
+  def add_student(name,grade)
+    # binding.pry
+    if @roster[grade]
+      @roster[grade] << name
+    else
+    @roster[grade]=[] 
+    @roster[grade] << name
+    end
+  
+  end
+  
+  def grade(num)
+     @roster[num]
+  end
+
+  def sort
+    #@roster.each_value {|value| puts value}
+    # @roster.each {|key,value|} 
+    #     puts @roster[key]
+    # @roster.each_value do |value|
+    #     value.sort
+       
+    #   end
+    
+     #@roster.each {|key, value| puts "#{key} is #{value}" }
+     @roster.each {|key, value| value.sort! }
+
+   # binding.pry
+  end
+
+
+end
